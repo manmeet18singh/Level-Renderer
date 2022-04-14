3376b20c26f4e8f9d1ebc3fd7aa20c5ec37ba56c
Manmeet Singh <singh18manmeet@gmail.com>
2022-04-14 17:29:38 -0400

    Revert "Part 4 Completed"
    This reverts commit fbb294b911da73781ad277192f2e90479049bfea.

--- main.cpp
+++ main.cpp
@@ -2,13 +2,15 @@
 #define GATEWARE_ENABLE_CORE // All libraries need this
 #define GATEWARE_ENABLE_SYSTEM // Graphics libs require system level libraries
 #define GATEWARE_ENABLE_GRAPHICS // Enables all Graphics Libraries
-#define GATEWARE_ENABLE_MATH // Enables all Math
-// TODO: Part 3a
 // Ignore some GRAPHICS libraries we aren't going to use
 #define GATEWARE_DISABLE_GDIRECTX11SURFACE // we have another template for this
 #define GATEWARE_DISABLE_GDIRECTX12SURFACE // we have another template for this
 #define GATEWARE_DISABLE_GRASTERSURFACE // we have another template for this
 #define GATEWARE_DISABLE_GOPENGLSURFACE // we have another template for this
+// TODO: Part 2a
+#define GATEWARE_ENABLE_MATH 
+// TODO: Part 4a
+#define GATEWARE_ENABLE_INPUT
 // With what we want & what we don't defined we can include the API
 #include "Gateware/Gateware.h"
 #include "renderer.h"
@@ -26,9 +28,9 @@ int main()
 	if (+win.Create(0, 0, 800, 600, GWindowStyle::WINDOWEDBORDERED))
 	{
 		// TODO: Part 1a
-		win.SetWindowName("Manmeet Singh - Assignment 2 - VULKAN");
+		win.SetWindowName("Manmeet Singh - Assignment 1 - VULKAN");
 		VkClearValue clrAndDepth[2];
-		clrAndDepth[0].color = { {0.113f, 0.137f, 0.145f, 1} };
+		clrAndDepth[0].color = { {0.75f, 0.4f, 0.25f, 1} }; // TODO: Part 1a
 		clrAndDepth[1].depthStencil = { 1.0f, 0u };
 		msgs.Create([&](const GW::GEvent& e) {
 			GW::SYSTEM::GWindow::Events q;
@@ -54,6 +56,8 @@ int main()
 			{
 				if (+vulkan.StartFrame(2, clrAndDepth))
 				{
+					// TODO: Part 4b
+					renderer.UpdateCamera();
 					renderer.Render();
 					vulkan.EndFrame(true);
 				}
